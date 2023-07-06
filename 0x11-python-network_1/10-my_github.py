18432fad5170dbfffdbee49ad5975480bea384de
20A1 <ayshaowolabi@gmail.com>
2023-07-06 13:42:44 -0700

    github

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,11 @@
+#!/user/bin/python3
+first argument,user name
+import requests
+import sys
+
+
+username = { 'username':ayshaowolabi@gmail.com}
+password = { 'password': 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}
+
+
+
